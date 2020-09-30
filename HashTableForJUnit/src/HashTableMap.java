ff59910db94ef87fdd46c55310bbdaad6b09e96e
Ben Radesovich <bpradosevich@gmail.com>
2020-09-30 14:07:46 -0500

    Create HashTableMap.java

---
+++ HashTableForJUnit/src/HashTableMap.java
@@ -0,0 +1,45 @@
+import java.util.NoSuchElementException;
+
+public class HashTableMap<Key, Value > implements HashMapADT<Key, Value>{
+
+    @Override
+    public boolean put(Key key, Value value) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Value get(Key key) throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean containsKey(Key key) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Value remove(Key key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    
+
+
+}
