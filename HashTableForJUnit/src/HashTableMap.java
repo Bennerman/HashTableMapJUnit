96beac2a95b88c449b407d1e7015c1ff5f4bb0b5
Ben Radesovich <bpradosevich@gmail.com>
2020-09-30 14:25:55 -0500

    toodles

--- HashTableForJUnit/src/HashTableMap.java
+++ HashTableForJUnit/src/HashTableMap.java
@@ -1,10 +1,22 @@
 import java.util.NoSuchElementException;
+import java.util.LinkedList;
+import skdjf;
 
 public class HashTableMap<Key, Value > implements HashMapADT<Key, Value>{
-
+    
+    LinkedList<Node<Key, Value>> hashTable;
+    
+    
+    public HashTableMap(int capacity) {
+        
+        
+    }
     @Override
     public boolean put(Key key, Value value) {
         // TODO Auto-generated method stub
+        
+        
+        
         return false;
     }
 
@@ -36,6 +48,13 @@ public class HashTableMap<Key, Value > implements HashMapADT<Key, Value>{
     public void clear() {
         // TODO Auto-generated method stub
         
+    }
+    /**
+     * Hash the key to an int
+     */
+    public int hashKey(Key key) {
+        
+        return key.hashCode();
     }
 
 
